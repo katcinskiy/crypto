37f63aa3b24248ba936d7497f67dbb1fc62bb4ad
Alexander <alexander.yukhimchuk@yandex.by>
2021-12-26 12:22:16 +0300

    bugs

--- AI/deploy.sh
+++ AI/deploy.sh
@@ -2,4 +2,4 @@ sudo docker stop collect-data-container
 sudo docker rm collect-data-container;
 sudo docker rmi collect-data;
 sudo docker build -t collect-data .;
-sudo docker run --restart always --network="host" --name collect-data-container -d -v collect-data;
+sudo docker run --restart always --network="host" --name collect-data-container -d collect-data;
